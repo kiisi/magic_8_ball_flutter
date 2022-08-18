d8e1e55e939e7841b8710791a9c3028c87b638c9
kiisi <kiisifelix06@gmail.com>
2022-08-19 00:41:22 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Magic Ball'),
+          ),
+          body: MagicBall(),
+        ),
+      ),
+    );
+
+class MagicBall extends StatefulWidget {
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  var num = Random().nextInt(5) + 1;
+
+  void randomImage() {
+    setState(() {
+      num = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/ball$num.png'),
+              onPressed: () => {randomImage()},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
